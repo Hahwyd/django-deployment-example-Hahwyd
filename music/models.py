a78e065ee9ddd93c7ca64eb74462f5ae2ee97a89
miclovich <vicmiclovich@gmail.com>
2025-02-06 23:53:10 +0100

    chore: add string rep

--- music/models.py
+++ music/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Song(models.Model):
     title = models.CharField(max_length=255, blank=False)
     file = models.FileField(upload_to='uploads/')
+
+    def __str__(self):
+        return f"<title={self.title}, file={self.file}>"
