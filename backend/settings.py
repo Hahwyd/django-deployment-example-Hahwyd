bac23f9bf70f50eb44a8f346b40aaede020febc0
Hahwyd <162121051+Hahwyd@users.noreply.github.com>
2025-02-07 11:47:04 +0100

    Update settings.py
    Added allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -48,7 +48,7 @@ SECRET_KEY = 'django-insecure--=wd^psc3g#fsn8jy&)4$!px2h1np8kr&n#y5sbczq4^-^4d79
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['18.194.207.118',]
 
 
 # Application definition
